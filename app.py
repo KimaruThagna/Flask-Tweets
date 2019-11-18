db482b673bbf2294c050bf95d440a14d8fd2c944
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-18 21:54:07 +0300

    flask landing page

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask,render_template,request,jsonify
 import tweepy
 from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+app.run()
