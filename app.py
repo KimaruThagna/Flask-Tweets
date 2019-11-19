34c1564d11a138397f29c3b97ab1852ad0d29429
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-19 10:16:57 +0300

    twitter authentication

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,request,jsonify
-import tweepy
+import tweepy, os
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 app = Flask(__name__)
+# twitter authentication
+auth = tw.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
+api = tw.API(auth, wait_on_rate_limit=True)
 
 @app.route("/")
 def index():
