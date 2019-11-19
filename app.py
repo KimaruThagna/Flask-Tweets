2f3d27c92b11498171dcd1f5c188d573e845f162
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-19 15:20:07 +0300

    cleanup

--- app.py
+++ app.py
@@ -16,14 +16,12 @@ def index():
 @app.route("/search",methods=["POST"])
 def search():
     search_tweet = request.form.get("search_query")
-    # t = [[]]
     t = []
     tweets = api.search(search_tweet, tweet_mode='extended')
     for tweet in tweets:
         polarity = TextBlob(tweet.full_text).sentiment.polarity
         subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
         t.append([tweet.full_text,polarity,subjectivity])
-        # t.append(tweet.full_text)
 
     return jsonify({"success":True,"tweets":t})
 
