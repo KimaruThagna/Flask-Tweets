02c7fc5579ea7acbc5f23f4610fa895fc06858bf
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-19 15:23:37 +0300

    use Vader library to perform analysis

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tweepy as tw
 import os
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
 app = Flask(__name__)
 # twitter authentication
 auth = tw.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
@@ -18,11 +19,14 @@ def search():
     search_tweet = request.form.get("search_query")
     t = []
     tweets = api.search(search_tweet, tweet_mode='extended')
+
     for tweet in tweets:
         polarity = TextBlob(tweet.full_text).sentiment.polarity
         subjectivity = TextBlob(tweet.full_text).sentiment.subjectivity
         t.append([tweet.full_text,polarity,subjectivity])
+        # using Vader sentiment analysis library
+        score = analyzer.polarity_scores(tweet.full_text)  # obtain polarity index of given sentence.
 
-    return jsonify({"success":True,"tweets":t})
+    return jsonify({"success":True,"tweet_analysis_textBlob":t})
 
 app.run()
